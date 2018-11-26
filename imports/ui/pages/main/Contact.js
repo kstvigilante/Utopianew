f4288732b1b5928d570e1db8b21cd6ef4fd41f28
Amar <amarjss0094@gmail.com>
2018-11-26 12:33:25 +0530

    telegram Button

--- imports/ui/pages/main/Contact.js
+++ imports/ui/pages/main/Contact.js
@@ -1,2 +1,8 @@
 import "./Contact.html"
 import "../../stylesheets/Shop.css";
+
+Template.contact.events({
+    "click .contactbutton":function(){
+        window.open("https://t.me/UtopiaCountry","_blank")
+    }
+})
